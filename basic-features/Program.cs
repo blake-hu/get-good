b2a8888110cb452cbf54a062a934f1933fe03f90
Blake Hu <64202537+blake-hu@users.noreply.github.com>
2024-10-26 17:42:28 -0500

    Add basic-features and gitignore

---
+++ basic-features/Program.cs
@@ -0,0 +1,37 @@
+﻿var names = new List<string> {"Allen"};
+names.Add("Blake");
+names.Add("Diane");
+
+// iterating using foreach loop
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
+
+// iterating using for loop 
+for (int i = 0; i < names.Count; i++)
+{
+    Console.WriteLine(names[i]);
+}
+
+interface IRenderable
+{
+    public int Render(object gameObject);
+}
+
+interface IGameObject
+{
+    public int Collide(object gameObject);
+}
+
+class Monster : IRenderable, IGameObject
+{
+    public int Render(object gameObject)
+    {
+        return 1;
+    }
+    public int Collide(object gameObject)
+    {
+        return 1;
+    }
+}
